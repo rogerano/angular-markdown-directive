ed437dbb13f4925725de2b3ed90436cb5a1b05d1
rogerano <rogerano@gmail.com>
2013-06-27 10:31:14 +1000

    Fixed attribute watch

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # angular-markdown-directive
+Original component by [btford](https://github.com/btford/angular-markdown-directive).
 Bower Component for a simple AngularJS Markdown directive using [Showdown](https://github.com/coreyti/showdown). Based on [this excellent tutorial](http://blog.angularjs.org/2012/05/custom-components-part-1.html) by [@johnlinquist](https://twitter.com/johnlindquist).
 
 ## Usage
-1. `bower install angular-markdown-directive`
-2. Made sure the showdown lib is loaded. It should be installed as a dependency at `components/showdown/compressed/showdown.js`.
-3. Include the `markdown.js` script provided by this component into your app.
-4. Add `btford.markdown` as a module dependency to your app.
+Static display from content.
+    <markdown>**This text will be bold**</markdown>
+Dynamic update from attribute.
+    <textarea ng-init="mdSource='**This text will be bold**'" ng-model="{{mdSource}}"></textarea>
+    <markdown markdown="{{mdSource}}"></markdown>
 
 ## License
 MIT
--- markdown.js
+++ markdown.js
@@ -1,26 +1,35 @@
+(function()
+{
 /*
  * angular-markdown-directive v0.1.0
  * (c) 2013 Brian Ford http://briantford.com
  * License: MIT
+ * Contributions by Andre Buchanan.
  */
 
-'use strict';
+"use strict";
 
-angular.module('btford.markdown', []).
-  directive('btfMarkdown', function () {
+angular.module("btford.markdown", []).
+  directive("markdown", function () {
     var converter = new Showdown.converter();
     return {
-      restrict: 'AE',
+      restrict: "AE",
       link: function (scope, element, attrs) {
-        if (attrs.btfMarkdown) {
-          scope.$watch(attrs.btfMarkdown, function (newVal) {
-            var html = converter.makeHtml(newVal);
-            element.html(html);
-          });
-        } else {
-          var html = converter.makeHtml(element.text());
-          element.html(html);
+        // If the intention here is to monitor an attr and create markdown when the
+        // value of the attr changes, scope.$watch doesn"t work. Use attr.$observe
+        // as of 1.0.7.
+        attrs.$observe("markdown", function(newValue)
+        {
+          if (newValue === undefined) return;
+          var md = converter.makeHtml(newValue);
+          element.html(md);
+        });
+        if (element.text())
+        {
+          var md = converter.makeHtml(element.text());
+          element.html(md);
         }
       }
     };
   });
+})();
